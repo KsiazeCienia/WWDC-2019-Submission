fac6e4579d7fcf47d12c2c6599898710e7cab5cf
Marcin Włoczko <marcin@prismake.com>
2019-03-23 21:01:35 +0100

    AspetFill bojects in menu

--- ZeroWaste/MenuScene.swift
+++ ZeroWaste/MenuScene.swift
@@ -103,6 +103,7 @@ final class MenuScene: SKScene {
         let size = CGSize(width: 40, height: 80)
         trash.size = size
         trash.zRotation = CGFloat.random(in: 0 ... 360)
+        trash.aspectFillToSize(fillSize: size)
         let xPosition = GKRandomDistribution(lowestValue: Int(frame.minX),
                                              highestValue: Int(frame.maxX))
         trash.position = CGPoint(x: CGFloat(xPosition.nextInt()),
--- ZeroWaste/SKSpriteNodeExtensions.swift
+++ ZeroWaste/SKSpriteNodeExtensions.swift
@@ -28,5 +28,19 @@ extension SKSpriteNode {
         return scaleRatio
     }
 
+    func aspectFillToSize(fillSize: CGSize) {
+
+        if texture != nil {
+            self.size = texture!.size()
+
+            let verticalRatio = fillSize.height / self.texture!.size().height
+            let horizontalRatio = fillSize.width /  self.texture!.size().width
+
+            let scaleRatio = horizontalRatio > verticalRatio ? horizontalRatio : verticalRatio
+
+            self.setScale(scaleRatio)
+        }
+    }
+
 
 }
